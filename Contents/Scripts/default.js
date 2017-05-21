51c61797fc6292d1e817211cd9cab7a7d277c01b
Lukas Pustina <lukas@pustina.net>
2017-05-21 21:23:54 +0200

    PoC for dynamic menu structure

--- Contents/Scripts/default.js
+++ Contents/Scripts/default.js
@@ -1,9 +1,53 @@
-function run(argument) {
-  LaunchBar.executeAppleScriptFile('activateEvernote.applescript');
-}
+
+let SETTINGS_FILE = Action.path + '/Contents/Scripts/settings.js'
 
 function runWithString(query) {
-  createNewNote = LaunchBar.options.shiftKey ? 1 : 0;
+  LaunchBar.log("runWithStrings");
+  LaunchBar.log(JSON.stringify(query));
+  //createNewNote = LaunchBar.options.shiftKey ? 1 : 0;
 
-  LaunchBar.executeAppleScriptFile('openNote.applescript', query, createNewNote);
+  //LaunchBar.executeAppleScriptFile('openNote.applescript', query, createNewNote);
+
+  if (query.length > 0) {
+    return [
+      { title: "Result 1: " + query },
+      { title: "Result 2: " + query },
+      { title: "Result 3: " + query },
+      { title: "Result 4: " + query }
+    ];
+  } else {
+    return [
+      { title: "Saved Searches", action: 'saved_searches', actionReturnsItems: true },
+      { title: "Create new Note" },
+      { title: "Edit Settings", path: SETTINGS_FILE }
+    ];
+  }
+
+}
+
+function runWithItem(item) {
+  LaunchBar.log("runWithItem");
+  LaunchBar.log(JSON.stringify(item));
+}
+
+function saved_searches(argument) {
+  LaunchBar.log("saved_searches");
+  LaunchBar.log(JSON.stringify(arguments));
+  settings = loadSettings(SETTINGS_FILE);
+
+  return settings.saved_searches;
+}
+
+function search(query) {
+  return [
+    { title: "Result 1: " + query },
+    { title: "Result 2: " + query },
+    { title: "Result 3: " + query },
+    { title: "Result 4: " + query }
+  ];
+}
+
+function loadSettings(settingsFile) {
+  let object = File.readJSON(settingsFile);
+  return object;
 }
