9ea1738ac9f875c0c1ba0fd8333dcd18b7d8b3b4
Lukas Pustina <lukas@pustina.net>
2017-05-31 21:00:09 +0200

    Update example settings

--- Contents/Scripts/settings.js
+++ Contents/Scripts/settings.js
@@ -1,6 +1,8 @@
 {
   "debug": true,
   "saved_searches": [
+    { "name": "Arbeitstagebuch", "search": "intitle:\"Arbeitstagebuch\"" },
+    { "name": "GF Vorgänge", "search": "intitle:\"GF Vorgang\"" },
     { "name": "Vorgänge", "search": "intitle:\"Vorgang\"" },
     { "name": "Vorgänge done", "search": "intitle:\"Vorgang\" intitle:\"done\"" }
   ]
